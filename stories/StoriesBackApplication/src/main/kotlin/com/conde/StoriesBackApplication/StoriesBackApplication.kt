df2709659dd60c645234a8d367b72563bda18853
Andrés Conde <andres.conde@innocv.com>
2023-10-01 16:26:34 +0200

    initial commit

---
+++ stories/StoriesBackApplication/src/main/kotlin/com/conde/StoriesBackApplication/StoriesBackApplication.kt
@@ -0,0 +1,11 @@
+package com.andresConde.StoriesBackApplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoriesBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<StoriesBackApplication>(*args)
+}
---
+++ stories/StoriesBackApplication/src/test/kotlin/com/andres_conde/StoriesBackApplication/StoriesBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.andresConde.StoriesBackApplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoriesBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
