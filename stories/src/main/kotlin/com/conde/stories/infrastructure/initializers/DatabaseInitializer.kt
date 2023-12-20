94dbd8e67984fdfa7f5c2849b65aacdcf1968f72
Andrés Conde <andres.conde@innocv.com>
2023-12-20 23:18:33 +0100

    stories sql schemas implemented

--- stories/src/main/kotlin/com/conde/stories/infrastructure/initializers/DatabaseInitializer.kt
+++ stories/src/main/kotlin/com/conde/stories/infrastructure/initializers/DatabaseInitializer.kt
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component
 class DatabaseInitializer(private val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
     override fun run(vararg args: String?) {
         initializeGreetings()
+        initializeUsers()
+        initializeHistory()
     }
 
     private fun initializeGreetings() {
@@ -20,5 +22,52 @@ class DatabaseInitializer(private val jdbcTemplate: JdbcTemplate) : CommandLineR
 		""".trimIndent())
         jdbcTemplate.execute("DELETE FROM greetings WHERE id = '0'")
         jdbcTemplate.execute("INSERT INTO greetings (id, text, number) VALUES ('0', 'Hello world', 987)")
+    }
+
+    private fun initializeUsers() {
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+				id VARCHAR(60) PRIMARY KEY,
+				name VARCHAR(255) NOT NULL
+			)
+        """.trimIndent())
+    }
+
+    private fun initializeHistory() {
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS stories (
+				id VARCHAR(60) PRIMARY KEY,
+				title VARCHAR(255) NOT NULL,
+                startDate BIGINT NOT NULL,
+                endDate BIGINT,
+                version INT
+			)
+        """.trimIndent())
+
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS element (
+				id VARCHAR(60) PRIMARY KEY,
+                historyId VARCHAR(60),
+                FOREIGN KEY (historyId) REFERENCES stories(id)
+			)
+        """.trimIndent())
+
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS text (
+				id VARCHAR(60) PRIMARY KEY,
+				text VARCHAR(4095) NOT NULL,
+                elementId VARCHAR(60),
+                FOREIGN KEY (elementId) REFERENCES element(id)
+			)
+        """.trimIndent())
+
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS image (
+				id VARCHAR(60) PRIMARY KEY,
+				imageUrl VARCHAR(1023) NOT NULL,
+                elementId VARCHAR(60),
+                FOREIGN KEY (elementId) REFERENCES element(id)
+			)
+        """.trimIndent())
     }
 }
